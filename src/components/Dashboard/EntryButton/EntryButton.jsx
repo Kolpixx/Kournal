0278a16212cc4f22cadb5b6c79b0e2b17128c95b
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2025-09-17 19:29:16 +0200

    fix currentEntryKey not being set before editing file directly from EntryButton component

--- src/components/Dashboard/EntryButton/EntryButton.jsx
+++ src/components/Dashboard/EntryButton/EntryButton.jsx
@@ -1,11 +1,11 @@
 import './EntryButton.css'
 
-export default function EntryButton(currentEntrykey, showDiaryInformation, entry, toggleEditingMode, showCreationPage) {
+export default function EntryButton(currentEntryKey, showDiaryInformation, entry, toggleEditingMode, showCreationPage) {
     return (
         <div key={entry.key} className="entry" onClick={() => viewEntry(currentEntrykey, showDiaryInformation, entry.key)}>
             <h1>{entry.emoji}</h1>
             <p>{entry.date}</p>
-            <button className="edit" onClick={(e) => {e.stopPropagation(); toggleEditingMode(true); showCreationPage(true)}}>
+            <button className="edit" onClick={(e) => {e.stopPropagation(); currentEntryKey.current = entry.key; toggleEditingMode(true); showCreationPage(true)}}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="#262626" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
             </button>
         </div>
