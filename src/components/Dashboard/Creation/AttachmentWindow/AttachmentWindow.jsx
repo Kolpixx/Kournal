4cc825efdf736c02e200fd6936ffcdd68714a095
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2025-09-23 19:55:25 +0200

    update AttachmentWIndow.jsx

--- src/components/Dashboard/Creation/AttachmentWindow/AttachmentWindow.jsx
+++ src/components/Dashboard/Creation/AttachmentWindow/AttachmentWindow.jsx
@@ -3,17 +3,18 @@ import './AttachmentWindow.css';
 import AttachImageButton from './images/AttachImagesButton';
 import { getImages } from '../../../../util/getImages';
 import { useState, useEffect } from 'react';
+import { getCurrentDate } from '../../../../util/getCurrentDate';
 
 export default function AttachmentWindow({toggleAttachmentWindow}) {
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
 
-    const refreshImages = () => {
-        setImages(getImages() || []);
-    }
+    // const refreshImages = () => {
+    //     setImages(getImages() || []);
+    // }
 
-    useEffect(() => {
-        refreshImages();
-    }, []);
+    // useEffect(() => {
+    //     refreshImages();
+    // }, []);
 
     return (
         <div id="attachment-window" onClick={(e) => {e.target.id === "attachment-window" && toggleAttachmentWindow(false)}}>
@@ -22,11 +23,73 @@ export default function AttachmentWindow({toggleAttachmentWindow}) {
                 <section id="attachment-window-images">
                     <h3>Images</h3>
                     <div id="attachment-window-images-list">
-                        <AttachImageButton refreshImages={refreshImages} />
-                        {images}
+                        {/* <AttachImageButton refreshImages={refreshImages} />
+                        {images} */}
+                        <button onClick={addImage}>Add Image</button>
+                        <button onClick={retrieveImages}>Retrieve Images</button>
                     </div>
                 </section>
             </div>
         </div>
     )
+}
+
+function addImage() {
+    console.log("Running addImage...");
+    
+    // Get current date
+    const date = getCurrentDate("YYYYMMDD");
+    console.log("Current Date:", date);
+
+    const indexedDB =
+        window.indexedDB ||
+        window.mozIndexedDB ||
+        window.webkitIndexedDB ||
+        window.msIndexedDB ||
+        window.shimIndexedDB;
+
+    if (!indexedDB) {
+        console.error("Your browser doesn't seem to support indexedDB :/");
+    }
+
+    // Create input
+    const input = document.createElement("input");
+    input.type = "file";
+
+    input.onchange = () => {
+        // Get selectedFile object
+        const selectedFile = input.files[0];
+
+        // Get base64 of file
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(selectedFile);
+
+        fileReader.onload = () => {
+            const base64 = fileReader.result;
+            console.log("Base64:", base64);
+
+            // Fun Part: IndexedDB!!
+            // Create Database if it doesn't exist yet
+            const request = indexedDB.open("EntriesAttachments", 1);
+
+            // Catch error
+            request.onerror = (e) => {
+                console.log("Failed to create/open database", e);
+            }
+
+            // Create basic structure
+            request.onupgradeneeded = () => {
+                console.log("Creating basic database structure");
+                const db = request.result;
+                db.createObjectStore()
+            }
+        }
+    }
+
+    input.click();
+
+}
+
+function retrieveImages() {
+    console.log("retrieveImages");
 }
